d1db3d5a8d775d6cfd1720e4d490da94037a3b60
ZGZ <301082566@qq.com>
2020-12-05 00:55:51 +0800

    完成文件上传demo

--- web/src/view/demo_upload/index.vue
+++ web/src/view/demo_upload/index.vue
@@ -10,58 +10,39 @@
             :on-error="uploadError"
             :on-success="uploadSuccess"
             :show-file-list="false"
+            multiple
           >
             <el-button size="small" type="primary">点击上传</el-button>
-            <div class="el-upload__tip" slot="tip">
-              只能上传jpg/png文件，且不超过500kb
-            </div>
+            <div class="el-upload__tip" slot="tip">可以上传所有格式文件，单个文件大小不超过1024Mb</div>
           </el-upload>
-        </el-col>
-        <el-col :span="12">
-          带压缩的上传, (512(k)为压缩限制)
-          <upload-image v-model="imageUrl" :fileSize="512" :maxWH="1080" />
-          已上传文件 {{ imageUrl }}
         </el-col>
       </el-row>
 
-      <el-table :data="tableData" border stripe>
-        <el-table-column label="预览" width="100">
-          <template slot-scope="scope">
-            <CustomPic picType="file" :picSrc="scope.row.url" />
-          </template>
-        </el-table-column>
+      <el-row>
+        <el-button type="danger" size="small" @click="deleteFileSelected()">批量删除</el-button>
+      </el-row>
+
+      <el-table :data="tableData" border stripe @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55"></el-table-column>
         <el-table-column label="日期" prop="UpdatedAt" width="180">
           <template slot-scope="scope">
             <div>{{ scope.row.UpdatedAt | formatDate }}</div>
           </template>
         </el-table-column>
-        <el-table-column
-          label="文件名"
-          prop="name"
-          width="180"
-        ></el-table-column>
-        <el-table-column
-          label="链接"
-          prop="url"
-          min-width="300"
-        ></el-table-column>
+        <el-table-column label="文件名" prop="name" width="180"></el-table-column>
+        <el-table-column label="链接" prop="url" min-width="300"></el-table-column>
         <el-table-column label="标签" prop="tag" width="100">
           <template slot-scope="scope">
             <el-tag
               :type="scope.row.tag === 'jpg' ? 'primary' : 'success'"
               disable-transitions
-              >{{ scope.row.tag }}</el-tag
-            >
+            >{{ scope.row.tag }}</el-tag>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="160">
           <template slot-scope="scope">
-            <el-button @click="downloadFile(scope.row)" size="small" type="text"
-              >下载</el-button
-            >
-            <el-button @click="deleteFile(scope.row)" size="small" type="text"
-              >删除</el-button
-            >
+            <el-button @click="downloadFile(scope.row)" size="small" type="text">下载</el-button>
+            <el-button @click="deleteFileSingle(scope.row)" size="small" type="text">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -86,15 +67,10 @@ import infoList from "@/mixins/infoList";
 import { getFileList, deleteFile } from "@/api/fileUploadAndDownload";
 import { downloadImage } from "@/utils/downloadImg";
 import { formatTimeToStr } from "@/utils/date";
-import CustomPic from "@/components/customPic";
-import UploadImage from "@/components/upload/image.vue";
 export default {
-  name: "Upload",
+  name: "DemoUpload",
   mixins: [infoList],
-  components: {
-    CustomPic,
-    UploadImage,
-  },
+  components: {},
   data() {
     return {
       fullscreenLoading: false,
@@ -102,34 +78,66 @@ export default {
       path: path,
       tableData: [],
       imageUrl: "",
+      //////////////
+      multipleSelection: []
     };
   },
   computed: {
-    ...mapGetters("user", ["userInfo", "token"]),
+    ...mapGetters("user", ["userInfo", "token"])
   },
   filters: {
-    formatDate: function (time) {
+    formatDate: function(time) {
       if (time != null && time != "") {
         var date = new Date(time);
         return formatTimeToStr(date, "yyyy-MM-dd hh:mm:ss");
       } else {
         return "";
       }
-    },
+    }
   },
   methods: {
-    async deleteFile(row) {
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    async deleteFileSelected() {
       this.$confirm("此操作将永久删除文件, 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
-        type: "warning",
+        type: "warning"
+      })
+        .then(async () => {
+          for (let i = 0; i < this.multipleSelection.length; i++) {
+            const row = this.multipleSelection[i];
+            const res = await deleteFile(row);
+            if (res.code == 0) {
+              this.$message({
+                type: "success",
+                message: row.name + "删除成功!"
+              });
+            }
+          }
+
+          this.getTableData();
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    async deleteFileSingle(row) {
+      this.$confirm("此操作将永久删除文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       })
         .then(async () => {
           const res = await deleteFile(row);
           if (res.code == 0) {
             this.$message({
               type: "success",
-              message: "删除成功!",
+              message: row.name + "删除成功!"
             });
             this.getTableData();
           }
@@ -137,11 +145,15 @@ export default {
         .catch(() => {
           this.$message({
             type: "info",
-            message: "已取消删除",
+            message: "已取消删除"
           });
         });
     },
     checkFile(file) {
+      // 限制文件大小
+      return file.size / 1024 / 1024 < 1024;
+
+      /*
       this.fullscreenLoading = true;
       const isJPG = file.type === "image/jpeg";
       const isPng = file.type === "image/png";
@@ -155,13 +167,14 @@ export default {
         this.fullscreenLoading = false;
       }
       return (isPng || isJPG) && isLt2M;
+      */
     },
     uploadSuccess(res) {
       this.fullscreenLoading = false;
       if (res.code == 0) {
         this.$message({
           type: "success",
-          message: "上传成功",
+          message: res.data.file.name + "上传成功"
         });
         if (res.code == 0) {
           this.getTableData();
@@ -169,23 +182,23 @@ export default {
       } else {
         this.$message({
           type: "warning",
-          message: res.msg,
+          message: res.msg
         });
       }
     },
     uploadError() {
       this.$message({
         type: "error",
-        message: "上传失败",
+        message: "上传失败"
       });
       this.fullscreenLoading = false;
     },
     downloadFile(row) {
       downloadImage(row.url, row.name);
-    },
+    }
   },
   created() {
     this.getTableData();
-  },
+  }
 };
 </script>
